b385ddfdda5a204b9f2f726c09869cfbc0e8d49b
Mario Estrada <mario@bittorrent.com>
2012-09-12 10:46:03 -0700

    support for detecting any of the safari built in search engines

--- js/start.js
+++ js/start.js
@@ -3,7 +3,37 @@
     if(window.top === window)
     {
         var url = window.location.href
-        var matches = /q=([^&]+)/i.exec(url)
+
+        // http-//search.yahoo.com/search?ei=utf-8&fr=aaplw&p=hola+yahoo
+        // http-//www.bing.com/search?q=hola+bing&form=APMCS1
+        var safari_search_engines = {
+            google: {
+                referral: /client=safari/i, //.test(url)
+                query: /\bq=([^&]+)/i //.exec(url)
+            },
+            bing: {
+                referral: /form=APMCS1/i,
+                query: /\bq=([^&]+)/i
+            },
+            yahoo: {
+                referral: /fr=aaplw/i,
+                query: /\bp=([^&]+)/i
+            }
+        }
+
+        var is_safari_search = false
+        var matches = false
+
+        for(var engine in safari_search_engines)
+        {
+            matches = safari_search_engines[engine].query.exec(url)
+            is_safari_search = safari_search_engines[engine].referral.test(url)
+
+            if(is_safari_search)
+            {
+                break
+            }
+        }
 
         var handleMessage = function(msg)
         {
@@ -45,7 +75,7 @@
             }
         }
 
-        if(/client=safari/i.test(url) && !/_ok_=1/i.test(url) && matches && matches[1] && matches[1].charAt(1) !== '!')
+        if(is_safari_search && !/_ok_=1/i.test(url) && matches && matches[1]) //&& matches[1].charAt(1) !== '!'
         {
             safari.self.tab.dispatchMessage("getSites")
         }

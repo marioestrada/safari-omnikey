a9af49958a5416db1347f908db9af5428d1f1123
Mario Estrada <marioeg@fb.com>
2017-05-18 21:25:48 -0700

    1.5 Redux backed, lib updates, minor fixes:
    - Using redux for store management
    - Adds copy button for export data
    - Updates all libraries
    - Minor fixes and UI changes

--- omnikey.safariextension/js/defaults.js
+++ omnikey.safariextension/js/defaults.js
@@ -1,57 +1,53 @@
-// jshint asi: true
-var Omnikey = Omnikey || {}
+var Omnikey = Omnikey || {};
 
+Omnikey.STORAGE_KEY = 'sites';
 Omnikey.default_sites = [
-    {
-        key: 'amazon',
-        url: 'http://www.amazon.com/s/?tag=httpmariec-20&link_code=wsw&_encoding=UTF-8&search-alias=aps&field-keywords={search}&Submit.x=0&Submit.y=0&Submit=Go'
-    },
-    {
-        key: 'newegg',
-        url: 'http://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&N=-1&isNodeId=1&Description={search}&x=0&y=0'
-    },
-    {
-        key: 'zappos',
-        url: 'http://www.zappos.com/search?term={search}'
-    },
-    {
-        key: 'youtube',
-        url: 'http://www.youtube.com/results?search_query={search}'
-    },
-    {
-        key: 'maps',
-        url: 'https://maps.google.com/maps?hl=en&authuser=0&q={search}&ie=UTF-8'
-    },
-    {
-        key: 'ddg',
-        url: 'http://duckduckgo.com/?q={search}'
-    },
-    {
-        key: 'ebay',
-        url: 'http://www.ebay.com/sch/i.html?_trksid=p5197.m570.l1313&_nkw={search}&_sacat=0'
-    },
-    {
-        key: 'rt',
-        url: 'http://www.rottentomatoes.com/search/?search={search}'
-    },
-    {
-        key: 'imdb',
-        url: 'http://www.imdb.com/find?q={search}&s=all'
-    },
-    {
-        key: 'twitter',
-        url: 'https://twitter.com/search?q={search}'
-    },
-    {
-        key: 'wiki',
-        url: 'http://en.wikipedia.org/w/index.php?title=Special:Search&search={search}'
-    },
-    {
-        key: 'wolfram',
-        url: 'http://www.wolframalpha.com/input/?i={search}'
-    },
-    {
-        key: 'mp3',
-        url: 'http://www.amazon.com/s/?tag=httpmariec-20&link_code=wsw&_encoding=UTF-8&search-alias=digital-music&field-keywords={search}&Submit.x=0&Submit.y=0&Submit=Go'
-    }
-]
+  {
+    key: "amazon",
+    url: "http://www.amazon.com/s/?tag=httpmariec-20&link_code=wsw&_encoding=UTF-8&search-alias=aps&field-keywords={search}&Submit.x=0&Submit.y=0&Submit=Go"
+  },
+  {
+    key: "youtube",
+    url: "http://www.youtube.com/results?search_query={search}"
+  },
+  {
+    key: "twitter",
+    url: "https://twitter.com/search?q={search}"
+  },
+  {
+    key: "spotify",
+    url: "https://open.spotify.com/search/results/{%search}"
+  },
+  {
+    key: "ebay",
+    url: "http://www.ebay.com/sch/i.html?_trksid=p5197.m570.l1313&_nkw={search}&_sacat=0"
+  },
+  {
+    key: "wiki",
+    url: "http://en.wikipedia.org/w/index.php?title=Special:Search&search={search}"
+  },
+  {
+    key: "zappos",
+    url: "http://www.zappos.com/search?term={search}"
+  },
+  {
+    key: "maps",
+    url: "https://maps.google.com/maps?hl=en&authuser=0&q={search}&ie=UTF-8"
+  },
+  {
+    key: "rt",
+    url: "http://www.rottentomatoes.com/search/?search={search}"
+  },
+  {
+    key: "imdb",
+    url: "http://www.imdb.com/find?q={search}&s=all"
+  },
+  {
+    key: "ddg",
+    url: "http://duckduckgo.com/?q={search}"
+  },
+  {
+    key: "wolfram",
+    url: "http://www.wolframalpha.com/input/?i={search}"
+  }
+];
--- omnikey.safariextension/js/global.js
+++ omnikey.safariextension/js/global.js
@@ -1,80 +1,100 @@
 /* jshint asi: true */
-/* globals Backbone, safari, Omnikey */
-
-var _gaq = _gaq || []
+/* globals ga, safari, Omnikey */
 
 var trackEvent = function(data) {
-    if (safari.extension.settings.getItem('ga_enabled')) {
-        _gaq.push(['_trackEvent', 'Actions'].concat(data))
-    }
-}
+  if (!ga) {
+    console.error('`ga` is not available');
+    return;
+  }
+
+  if (safari.extension.settings.getItem('ga_enabled')) {
+    ga.apply(null, ['send', 'event', 'Actions'].concat(data));
+  }
+};
 
 var createUrl = function(url, query) {
-    var new_url
+  var new_url;
 
-    query = encodeURIComponent(query)
-    query = query.replace(/%20/g, '+')
+  query = encodeURIComponent(query);
+  query = query.replace(/%20/g, '+');
 
-    new_url = url.replace(/\{search\}/g, query)
-    new_url = new_url.replace(/\{%search\}/g, query.replace(/\+/g, '%20'))
+  new_url = url.replace(/\{search\}/g, query);
+  new_url = new_url.replace(/\{%search\}/g, query.replace(/\+/g, '%20'));
 
-    return new_url
-}
+  return new_url;
+};
 
 var handleQuery = function(e) {
-    var SavedSites = new Backbone.LocalStorage('omnikey-sites')
-    var SitesList = SavedSites.findAll()
+  var savedSites;
+  try {
+    savedSites = JSON.parse(localStorage.getItem(Omnikey.STORAGE_KEY))
+  } catch (e) {}
 
-    var sites = Omnikey.default_sites
+  var sites = Omnikey.default_sites;
 
-    var search_site = localStorage.default_search
+  var search_site = localStorage.default_search;
 
-    if (SitesList.length > 0) {
-        sites = {}
-        for(var i = 0; i < SitesList.length; i++) {
-            sites[SitesList[i].key] = SitesList[i].url
+  if (savedSites.length > 0) {
+    sites = {};
+    for (var i = 0; i < savedSites.length; i++) {
+      sites[savedSites[i].key] = savedSites[i].url;
 
-            if(SitesList[i].default) {
-                search_site = SitesList[i].url
-            }
-        }
+      if (savedSites[i].default) {
+        search_site = savedSites[i].url;
+      }
     }
+  }
 
-    var full_query = e.query
-    var parts = full_query.split(' ')
-    var key = parts[0]
-    var query = parts.splice(1).join(' ')
-    var search_url
+  var full_query = e.query;
+  var parts = full_query.split(' ');
+  var key = parts[0];
+  var query = parts.splice(1).join(' ');
+  var search_url;
 
-    if (sites[key]) {
-        e.preventDefault()
+  if (sites[key]) {
+    e.preventDefault();
 
-        search_url = createUrl(sites[key], query)
+    search_url = createUrl(sites[key], query);
 
-        safari.extension.globalPage.contentWindow.trackEvent(['Search:key', key + '|' + search_url.match('\/\/[^\/]+\/')])
+    safari.extension.globalPage.contentWindow.trackEvent([
+      'Search:key',
+      key + '|' + search_url.match('\/\/[^\/]+\/')
+    ]);
 
-        e.target.url = search_url
-    } else if (safari.extension.settings.getItem('bang_search_enabled') && key[0] === '!'){
-        e.preventDefault()
+    e.target.url = search_url;
+  } else if (
+    safari.extension.settings.getItem('bang_search_enabled') &&
+    key[0] === '!'
+  ) {
+    e.preventDefault();
 
-        safari.extension.globalPage.contentWindow.trackEvent(['Search:force', key])
+    safari.extension.globalPage.contentWindow.trackEvent(['Search:force', key]);
 
-        e.target.url = 'https://www.google.com/search?q=' + encodeURIComponent(full_query.slice(1))
-    } else if (search_site){
-        e.preventDefault()
+    e.target.url =
+      'https://www.google.com/search?q=' +
+      encodeURIComponent(full_query.slice(1));
+  } else if (search_site) {
+    e.preventDefault();
 
-        safari.extension.globalPage.contentWindow.trackEvent(['Search:default', search_site])
+    safari.extension.globalPage.contentWindow.trackEvent([
+      'Search:default',
+      search_site
+    ]);
 
-        e.target.url = createUrl(search_site, full_query)
-    }
-}
+    e.target.url = createUrl(search_site, full_query);
+  }
+};
 
-safari.application.addEventListener('message', function( e ) {
+safari.application.addEventListener(
+  'message',
+  function(e) {
     if (e.name === 'getSettings') {
-        e.target.page.dispatchMessage('setSettings', {
-            referral_enabled: safari.extension.settings.getItem('referral_enabled')
-        });
+      e.target.page.dispatchMessage('setSettings', {
+        referral_enabled: safari.extension.settings.getItem('referral_enabled')
+      });
     }
-}, false);
+  },
+  false
+);
 
-safari.application.addEventListener("beforeSearch", handleQuery, false)
+safari.application.addEventListener('beforeSearch', handleQuery, false);
